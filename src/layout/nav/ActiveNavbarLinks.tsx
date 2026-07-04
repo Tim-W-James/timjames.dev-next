92c505a3cf24785a7744c2be2bc224cb8e0d3479
TimJ <38764646+Tim-W-James@users.noreply.github.com>
2026-07-04 12:35:23 +1000

    fix: inactive navbar (#7)

--- src/layout/nav/ActiveNavbarLinks.tsx
+++ src/layout/nav/ActiveNavbarLinks.tsx
@@ -9,16 +9,19 @@ const ActiveNavbarLinks: React.FC<{ inHamburgerMenu: boolean }> = ({
 }) => {
   const pathname = usePathname();
 
+  const selectedRoute =
+    Object.values(ROUTES).find((routeConfig) =>
+      routeConfig.route === "/"
+        ? pathname === "/" || pathname === ""
+        : pathname.startsWith(routeConfig.route),
+    ) ?? ROUTES.home; // Default to home if no match is found
+
   return (
     <>
       {Object.values(ROUTES).map((routeConfig, index) => (
         <NavbarLink
           inHamburgerMenu={inHamburgerMenu}
-          isSelected={
-            routeConfig.route === "/"
-              ? pathname === "/" || pathname === ""
-              : pathname.startsWith(routeConfig.route)
-          }
+          isSelected={selectedRoute.route === routeConfig.route}
           key={routeConfig.title}
           label={routeConfig.title}
           order={index + 1}
